d57398673083f573228b8f20870a20d1cf3c9b6b
ShinobiWPS <axanderghemov@gmail.com>
2024-02-09 17:35:21 +0100

    feat: add ESlint plugins for A11y, react-hooks, and require comma-dangle

---
+++ .eslintrc.cjs
@@ -0,0 +1,67 @@
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	extends: [
+		'eslint:recommended', // adds some MORE rules by default
+		'plugin:@stylistic/all-extends',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/stylistic',
+		'plugin:import/recommended',
+		'plugin:import/typescript',
+		'plugin:react-hooks/recommended',
+		'plugin:jsx-a11y/recommended',
+		// "plugin:react/recommended"
+		// "plugin:vue/recommended"
+	],
+	plugins: [
+		'@typescript-eslint',
+		'import',
+		'@stylistic',
+	],
+	rules: {
+		'@stylistic/indent': [
+			'error',
+			'tab',
+		],
+		'@stylistic/quotes': [
+			'error',
+			'single',
+		],
+		'@stylistic/semi': [
+			'error',
+			'never',
+		],
+		'@stylistic/quote-props': [
+			'error',
+			'as-needed',
+		],
+		'@stylistic/object-curly-spacing': [
+			'error',
+			'always',
+		],
+		'@stylistic/comma-dangle': [
+			'error',
+			'always',
+		],
+	},
+	settings: {
+		'import/resolver': {
+			typescript: true,
+			node: {
+				extensions: [
+					'.js',
+					'.jsx',
+					'.ts',
+					'.tsx',
+					'.vue',
+				],
+			},
+		},
+	},
+}
