c7d92e7731afe1c8fe49d174d58f5806dd867950
Alexandru Gherasim <ShinobiWPS@users.noreply.github.com>
2025-02-10 15:49:26 +0100

    Update vite.config.ts for testing and TanStack Router

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import { VitePWA } from 'vite-plugin-pwa'
@@ -6,6 +8,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+		TanStackRouterVite({ autoCodeSplitting: true }),
     react(),
     basicSsl(),
     VitePWA({
@@ -15,4 +18,8 @@ export default defineConfig({
       },
     }),
   ],
+	test: { globals: true },
+	/* tsconfig.compilerOptions: "types": [
+      "vitest/globals"
+    ], */
 })
